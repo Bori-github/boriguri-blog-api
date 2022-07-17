50302fb13a981f0708c85c33ee8b0b5076c11fc0
Bori-github <qhflrnfl4324@gmail.com>
2022-07-17 13:47:07 +0900

    :sparkles: firebase 초기 설정

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import {initializeApp} from 'firebase-admin/app';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('combined'));
+
+initializeApp();
 
 app.get('/', (req: Request, res: Response) => {
   const name = process.env.NAME || 'World';
